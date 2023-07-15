54ee63f9d2b67bf022955838a6c20639d7a9b33a
mcold <mholodnuk@gmail.com>
2023-07-15 10:54:24 +0300

    Up-Down arrows

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"io/ioutil"
+	"strings"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/storage"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -16,6 +21,7 @@ type config struct {
 	Snips        Snippets
 	IDGroup      int
 	IDSnip       int
+	WhoActive    string
 }
 
 var cfg config
@@ -26,16 +32,69 @@ func main() {
 	win := a.NewWindow("Sniper")
 
 	edit, l_snips, l_groups := cfg.makeUI()
+	cfg.createMenuItems(win)
 
 	// set the content of the window
 	win.SetContent(container.NewHSplit(l_groups, container.NewHSplit(l_snips, edit)))
+
+	cfg.WhoActive = "Group"
+
+	cfg.setArrows(win)
 
 	win.Resize(fyne.Size{Width: 800, Height: 500})
 	win.CenterOnScreen()
 	win.ShowAndRun()
 }
 
+func (app *config) setArrows(win fyne.Window) {
+	win.Canvas().SetOnTypedKey(func(k *fyne.KeyEvent) {
+		if k.Name == "Up" {
+			if cfg.WhoActive == "Group" {
+				newID := cfg.IDGroup - 1
+				if newID >= 0 {
+					cfg.IDGroup = newID
+					cfg.ListGroup.Select(cfg.IDGroup)
+					cfg.IDSnip = 0
+					cfg.EditWidget.SetText(cfg.Snips.Groups[cfg.IDGroup].Snips[cfg.IDSnip].Code)
+					cfg.EditWidget.Refresh()
+				}
+			} else if cfg.WhoActive == "Snip" {
+				newID := cfg.IDSnip - 1
+				if newID >= 0 {
+					cfg.IDSnip = newID
+					cfg.ListSnip.Select(cfg.IDSnip)
+					cfg.EditWidget.SetText(cfg.Snips.Groups[cfg.IDGroup].Snips[cfg.IDSnip].Code)
+					cfg.EditWidget.Refresh()
+				}
+			}
+		}
+		if k.Name == "Down" {
+			if cfg.WhoActive == "Group" {
+				newID := cfg.IDGroup + 1
+				if cfg.ListGroup.Length() > newID {
+					cfg.IDGroup = newID
+					cfg.IDSnip = 0
+					cfg.ListGroup.Select(cfg.IDGroup)
+					cfg.EditWidget.SetText(cfg.Snips.Groups[cfg.IDGroup].Snips[cfg.IDSnip].Code)
+					cfg.EditWidget.Refresh()
+				}
+
+			} else if cfg.WhoActive == "Snip" {
+				newID := cfg.IDSnip + 1
+				if cfg.ListSnip.Length() > newID {
+					cfg.IDSnip = newID
+					cfg.ListSnip.Select(cfg.IDSnip)
+					cfg.EditWidget.SetText(cfg.Snips.Groups[cfg.IDGroup].Snips[cfg.IDSnip].Code)
+					cfg.EditWidget.Refresh()
+				}
+			}
+
+		}
+	})
+}
+
 func (app *config) refreshGroup(id int) {
+	app.WhoActive = "Group"
 	app.IDGroup = id
 	app.ListSnip.Length = func() int { return len(app.Snips.Groups[id].Snips) }
 	app.ListSnip.UpdateItem = func(ids widget.ListItemID, obj fyne.CanvasObject) {
@@ -45,15 +104,16 @@ func (app *config) refreshGroup(id int) {
 
 	// activate first element by default
 	app.IDSnip = 0
-	app.ListSnip.Select(0)
-	app.refreshSnip(app.IDSnip)
+	// app.ListSnip.Select(0)
+	app.EditWidget.SetText(app.Snips.Groups[app.IDGroup].Snips[app.IDSnip].Code)
+	// app.refreshSnip(app.IDSnip)
 }
 
 func (app *config) refreshSnip(id int) {
+	app.WhoActive = "Snip"
 	app.IDSnip = id
 	app.EditWidget.SetText(app.Snips.Groups[app.IDGroup].Snips[app.IDSnip].Code)
 	app.EditWidget.Refresh()
-
 }
 
 func (app *config) makeUI() (*widget.Entry, *widget.List, *widget.List) {
@@ -96,4 +156,107 @@ func (app *config) makeUI() (*widget.Entry, *widget.List, *widget.List) {
 	app.ListGroup = l_groups
 
 	return edit, l_snips, l_groups
+}
+
+func (app *config) createMenuItems(win fyne.Window) {
+
+	// create three menu items
+	openMenuItem := fyne.NewMenuItem("Open...", app.openFunc(win))
+	saveMenuItem := fyne.NewMenuItem("Save", app.saveFunc(win))
+	app.SaveMenuItem = saveMenuItem
+	app.SaveMenuItem.Disabled = true
+	saveAsMenuItem := fyne.NewMenuItem("Save as...", app.saveAsFunc(win))
+
+	// create a file menu, and add the three items to it
+	fileMenu := fyne.NewMenu("File", openMenuItem, saveMenuItem, saveAsMenuItem)
+
+	// create a main menu, and add the file menu to it
+	menu := fyne.NewMainMenu(fileMenu)
+
+	// set the main menu for the application
+	win.SetMainMenu(menu)
+}
+
+var filter = storage.NewExtensionFileFilter([]string{".xml", ".XML"})
+
+func (app *config) saveFunc(win fyne.Window) func() {
+	return func() {
+		if app.CurrentFile != nil {
+			write, err := storage.Writer(app.CurrentFile)
+			if err != nil {
+				dialog.ShowError(err, win)
+				return
+			}
+
+			write.Write([]byte(app.EditWidget.Text))
+			defer write.Close()
+		}
+	}
+}
+
+func (app *config) openFunc(win fyne.Window) func() {
+	return func() {
+		openDialog := dialog.NewFileOpen(func(read fyne.URIReadCloser, err error) {
+			if err != nil {
+				dialog.ShowError(err, win)
+				return
+			}
+
+			if read == nil {
+				return
+			}
+
+			defer read.Close()
+
+			data, err := ioutil.ReadAll(read)
+			if err != nil {
+				dialog.ShowError(err, win)
+				return
+			}
+
+			app.EditWidget.SetText(string(data))
+
+			app.CurrentFile = read.URI()
+			win.SetTitle(win.Title() + " - " + read.URI().Name())
+			app.SaveMenuItem.Disabled = false
+
+		}, win)
+
+		openDialog.SetFilter(filter)
+		openDialog.Show()
+	}
+}
+
+func (app *config) saveAsFunc(win fyne.Window) func() {
+	return func() {
+		saveDialog := dialog.NewFileSave(func(write fyne.URIWriteCloser, err error) {
+			if err != nil {
+				dialog.ShowError(err, win)
+				return
+			}
+
+			if write == nil {
+				// user cancelled
+				return
+			}
+
+			if !strings.HasSuffix(strings.ToLower(write.URI().String()), ".xml") {
+				dialog.ShowInformation("Error", "Please name your file with a .xml extension!", win)
+				return
+			}
+
+			// save file
+			write.Write([]byte(app.EditWidget.Text))
+			app.CurrentFile = write.URI()
+
+			defer write.Close()
+
+			win.SetTitle(win.Title() + " - " + write.URI().Name())
+			app.SaveMenuItem.Disabled = false
+
+		}, win)
+		saveDialog.SetFileName("UserSnippets.xml")
+		saveDialog.SetFilter(filter)
+		saveDialog.Show()
+	}
 }
