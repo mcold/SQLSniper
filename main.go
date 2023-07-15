8c0cfa1ad8bdf925050636a95c01137add937de3
mcold <mholodnuk@gmail.com>
2023-07-15 09:13:19 +0300

    Edit refresh

--- main.go
+++ main.go
@@ -14,6 +14,8 @@ type config struct {
 	CurrentFile  fyne.URI
 	SaveMenuItem *fyne.MenuItem
 	Snips        Snippets
+	IDGroup      int
+	IDSnip       int
 }
 
 var cfg config
@@ -22,7 +24,6 @@ func main() {
 	a := app.New()
 
 	win := a.NewWindow("Sniper")
-	// win.Resize(fyne.NewSize(600, 600))
 
 	edit, l_snips, l_groups := cfg.makeUI()
 
@@ -34,12 +35,25 @@ func main() {
 	win.ShowAndRun()
 }
 
-func (app *config) refresh(id int) {
+func (app *config) refreshGroup(id int) {
+	app.IDGroup = id
 	app.ListSnip.Length = func() int { return len(app.Snips.Groups[id].Snips) }
 	app.ListSnip.UpdateItem = func(ids widget.ListItemID, obj fyne.CanvasObject) {
 		obj.(*fyne.Container).Objects[0].(*widget.Label).SetText(app.Snips.Groups[id].Snips[ids].Name)
 	}
 	app.ListSnip.Refresh()
+
+	// activate first element by default
+	app.IDSnip = 0
+	app.ListSnip.Select(0)
+	app.refreshSnip(app.IDSnip)
+}
+
+func (app *config) refreshSnip(id int) {
+	app.IDSnip = id
+	app.EditWidget.SetText(app.Snips.Groups[app.IDGroup].Snips[app.IDSnip].Code)
+	app.EditWidget.Refresh()
+
 }
 
 func (app *config) makeUI() (*widget.Entry, *widget.List, *widget.List) {
@@ -57,7 +71,7 @@ func (app *config) makeUI() (*widget.Entry, *widget.List, *widget.List) {
 			return 0
 		},
 		func() fyne.CanvasObject {
-			return container.NewHBox(widget.NewLabel("Template Object"))
+			return container.NewHBox(widget.NewLabel(""))
 		},
 		func(id widget.ListItemID, obj fyne.CanvasObject) {},
 	)
@@ -67,53 +81,19 @@ func (app *config) makeUI() (*widget.Entry, *widget.List, *widget.List) {
 			return len(app.Snips.Groups)
 		},
 		func() fyne.CanvasObject {
-			return container.NewHBox(widget.NewLabel("Template Object"))
+			return container.NewHBox(widget.NewLabel(""))
 		},
 		func(idg widget.ListItemID, obj fyne.CanvasObject) {
 			obj.(*fyne.Container).Objects[0].(*widget.Label).SetText(app.Snips.Groups[idg].Category)
-
-			// l_snips.Length = func() int {
-			// 	return len(snippets.Groups[idg].Snippets)
-			// }
-
-			//l_snips.UpdateItem = func(ids widget.ListItemID, obj fyne.CanvasObject) {
-			// obj.(*fyne.Container).Objects[0].(*widget.Label).SetText(snippets.Groups[idg].Snippets[ids].Name)
-
-			// etr_code.SetText(snippets.Groups[idg].Snippets[ids].Code)
-
-			// cnt = cnt + 1
-			// // str := "idg: %s", idg
-			// etr_code.SetText(fmt.Sprintf("idg: %s\nids: %s\ncnt: %s\n\ncode: %s", idg, ids, cnt, snippets.Groups[idg].Snippets[ids].Code))
-			// fmt.Println("idg: %s", idg)
-			// fmt.Println("l_groups:")
-			// fmt.Println("idg: ", idg)
-			// fmt.Println("ids: ", ids)
-			// fmt.Println(snippets.Groups[idg].Snippets[ids].Code)
-			// fmt.Println("cnt: ", cnt)
-			// fmt.Println()
-
-			//}
-			// l_snips.UnselectAll()
-			// l_snips.Select(0)
-
-			//l_snips.Resize(fyne.NewSize(200, 600))
-			// l_snips.Show()
-			// etr_code.Show()
-
 		},
 	)
 
-	l_groups.OnSelected = app.refresh
+	l_groups.OnSelected = app.refreshGroup
+	l_snips.OnSelected = app.refreshSnip
 
 	app.EditWidget = edit
 	app.ListSnip = l_snips
 	app.ListGroup = l_groups
 
 	return edit, l_snips, l_groups
-
-	// app.PreviewWidget = preview
-
-	// edit.OnChanged = preview.ParseMarkdown
-
-	// return edit, preview
 }
